0574a8d5f49838c65cbd295717731acd680b1a54
Sushanth Kurdekar <sushanth.uk@gmail.com>
2021-09-30 11:00:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import os
+from keep_alive import keep_alive
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('ping'):
+        await message.channel.send('pong!')
+
+
+client.run(os.getenv('TOKEN'))
